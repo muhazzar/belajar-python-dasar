9f0ac69a1f3895ac8b758910e5d71694c17e56cc
ijay <muhazzar639@gmail.com>
2025-02-11 20:59:19 +0700

    menambahkan file variabel

---
+++ PELAJARAN 01-VARIABEL/main.py
@@ -0,0 +1,21 @@
+#variabel adalah tempat menyimpan data
+
+#ASSIGMENT NILAI
+a = 1
+b = 2
+print('nilai a =',a)
+print('nilai b =',b)
+
+
+#ASSIGMENT INDIRECT
+c = b
+print('nilai c =',c)
+
+
+#PENAMAAN VARIABEL
+
+#tidak bisa memakai spasi :(data a = 1234)
+data_a = 1234 #spasi diganti menggunakan ( _ )
+
+#tidak bisa diawali dengan angka :(10data = 1234)
+data10 = 1234 #ini bissa
