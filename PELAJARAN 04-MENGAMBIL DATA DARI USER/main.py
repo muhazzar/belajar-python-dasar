8e3ab8b92ea22e223f23f1e1a02af968a431a86c
muhazzar <muhazzar639@gmail.com>
2025-02-12 19:43:01 +0700

    menambahkan file pelajaran input data user

---
+++ PELAJARAN 04-MENGAMBIL DATA DARI USER/main.py
@@ -0,0 +1,14 @@
+data_01 = input("masukan data :")#data yang dimasukan tipe datanya pasti string
+print("data :",data_01,type (data_01))
+
+#casting ke integer
+data_02 = int(input("masukan angka :"))#jika sudah di casting maka data yang dimasukan harus angka
+print("data :",data_02,type(data_02))
+
+#casting ke float
+data_03 = float(input("masukan nomor :"))#jika sudah di casting maka data yang dimasukan harus angka
+print("data :",data_03,type(data_03))
+
+#casting ke boolean
+data_04 = bool(int(input("masukan data :")))#tambahkan int agar bisa terbaca
+print("data :",data_04,type(data_04))
