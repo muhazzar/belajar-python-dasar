f403a2d60995d9e8ea616259d0a9fa64907ab501
muhazzar <muhazzar639@gmail.com>
2025-02-12 21:15:16 +0700

    menambahkan file belajar operasi aritmatika

---
+++ PELAJARAN 05-OPERASI ARITMATIKA/main.py
@@ -0,0 +1,30 @@
+a=11
+b=5
+
+#kali*
+print("hasil kali :",a*b)
+
+#bagi/
+print("hasil bagi :",a/b)
+
+#tambah+
+print("hasil tambah :",a+b)
+
+#kurang-
+print("hasil kurang :",a-b)
+
+#eksponen (pangkat)
+print("hasil pangkat :",a**b)
+
+#modulus
+print("hasil modulus :",a%b)
+
+#floor division
+print("hasil floor div :",a//b)
+
+##prioritas operasi, operational precedence
+'''
+    1.()
+    2.perkalian&friends *,/,**,%,//
+    3.+,-
+'''
