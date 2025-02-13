ab40ca7de1a92d346df4e6e87150640ba1bba582
muhazzar <muhazzar639@gmail.com>
2025-02-13 17:49:44 +0700

    menambahkan file belajar operasi komparasi

---
+++ PELAJARAN 07-OPERASI KOMPARASI/main.py
@@ -0,0 +1,83 @@
+#Setiap hasil dari operasi komparasi adalah boolean
+
+a=4
+b=2
+#lebih besar dari (>)
+print("LEBIH DARI")
+
+hasil = a > b
+print("a > b :",hasil)
+
+hasil = b > a
+print("b > a :",hasil)
+
+hasil = a > a
+print("a > a :",hasil)
+
+
+#kurang dari (<)
+print("\nKURANG DARI")
+
+hasil = a < b
+print("a < b :",hasil)
+
+hasil = b < a 
+print("b < a :",hasil)
+
+hasil =a < a
+print("a < a :",hasil)
+
+
+#LEBIH BESAR DARI =
+print("\nLEBIH DARI =")
+
+hasil = a >= b
+print("a >= b :",hasil)
+
+hasil = b >= a
+print("b >= a :",hasil)
+
+hasil = a >= a
+print("a >= a :",hasil)
+
+hasil = b >= b
+print("b >= b :",hasil)
+
+
+#KURANG DARI SAMADENGAN <=
+print("\nKURANG DARI =")
+
+hasil = a <= b
+print("a <= b",hasil)
+
+hasil = b <= a
+print("b <= a",hasil)
+
+hasil = a <= a
+print("a <= a",hasil)
+
+hasil = b <= b
+print("b <= b",hasil)
+
+
+#SAMA DENGAN ==
+print("\nSAMA DENGAN ==")
+
+hasil = a == b 
+print("a == b",hasil)
+
+hasil = a == a
+print("a == a",hasil)
+
+
+#TIDAK SAMA DENGAN !=
+print("\nTIDAK SAMA DENGAN !=")
+
+hasil = a != b 
+print("a != b",hasil)
+
+hasil = a != a
+print("a != a",hasil)
+
+
+# "is" dan "is not" belum understot
