4b91b5d5c494fa2e523048cb721b7bc78d317543
muhazzar <muhazzar639@gmail.com>
2025-02-25 11:09:08 +0700

    just modified

--- PELAJARAN 11-IF,ELIF,ELSE/elif.py
+++ PELAJARAN 11-IF,ELIF,ELSE/elif.py
@@ -1,16 +1,17 @@
 # membuat kalkulator menggunakan elif
 
 input01 = int(input("masukan angka pertama: "))
-input02 = int(input("masukan angka kedua: "))
 input03 = input("masukan jenis operasi: ")
+input02 = int(input("masukan angka kedua: "))
 
 if input03 == "*":
-    print("hasil:",input01 * input02)
+    print(input01,'x',input02,'=',input01 * input02)
 elif input03 == "/":
-    print(input01 / input02)
+    hasil = input01 / input02
+    print(f"{input01} / {input02} =",hasil)
 elif input03 == "-":
-    print(input01 - input02)
+    print(f"{input01} - {input02} =",input01 - input02)
 elif input03 == "+":
-    print(input01 + input02)
+    print(f"{input01} + {input02} =",input01 + input02)
 else :
     print("masukan operasi yang benar!! (*,/,-,+)")
